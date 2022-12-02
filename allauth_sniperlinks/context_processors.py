261c6ffa55b68e3036dcc1f2bd03ea3d41b93481
skulegirl <anna@egate.net>
2022-12-02 14:18:18 -0500

    Fix sniperlink context processor when user is not authenticated

--- allauth_sniperlinks/context_processors.py
+++ allauth_sniperlinks/context_processors.py
@@ -9,23 +9,26 @@ def unverified_email_sniperlinks(request):
     Adds sniperlinks for any unverified emails for the currently logged in user
     to context.
     """
-    sniperlinks_context = cache.get(USER_SNIPERLINK_CACHE_KEY.format(request.user.id))
-    if sniperlinks_context is None:
-        sniperlinks_context = {}
-        unverified_email_sls = SniperLink.objects.filter(
-            email_object__user=request.user, 
-            email_object__verified=False
-        )
-        for sl in unverified_email_sls:
-            sniperlinks_context[sl.email_object.email] = {
-                'link': get_sniper_link(sl.mail_provider, sl.email_object.email, VERIFICATION_SENDER),
-                'img': get_sniper_link_img(sl.mail_provider)
-            }
-        # Cache entry is invalidated via signal handler when any of the user's email entries change
-        cache.set(
-            USER_SNIPERLINK_CACHE_KEY.format(request.user.id),
-            sniperlinks_context,
-        )
-    return {
-        'sniperlinks': sniperlinks_context
-    }
+    if request.user.is_authenticated:
+        sniperlinks_context = cache.get(USER_SNIPERLINK_CACHE_KEY.format(request.user.id))
+        if sniperlinks_context is None:
+            sniperlinks_context = {}
+            unverified_email_sls = SniperLink.objects.filter(
+                email_object__user=request.user, 
+                email_object__verified=False
+            )
+            for sl in unverified_email_sls:
+                sniperlinks_context[sl.email_object.email] = {
+                    'link': get_sniper_link(sl.mail_provider, sl.email_object.email, VERIFICATION_SENDER),
+                    'img': get_sniper_link_img(sl.mail_provider)
+                }
+            # Cache entry is invalidated via signal handler when any of the user's email entries change
+            cache.set(
+                USER_SNIPERLINK_CACHE_KEY.format(request.user.id),
+                sniperlinks_context,
+            )
+        return {
+            'sniperlinks': sniperlinks_context
+        }
+    else:
+        return {}
